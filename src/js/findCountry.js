aafb7413de3d633a7e40fac2d9fe31957b17adbb
Yehor Tuz <tuzegor@gmail.com>
2021-10-15 19:18:09 +0300

    работает лист и формирует блок одной страны

--- src/js/findCountry.js
+++ src/js/findCountry.js
@@ -1,45 +1,45 @@
 // Есть файл fetchCountries.js с дефолтным экспортом функции fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
 
-import refs from './refs.js'
-const {userCountry, listCountry, cardCountry} = refs;
-import fetchCountries from './fetchCountries.js'
+import refs from './refs.js';
+const { userCountry, listCountry, cardCountry } = refs;
+import fetchCountries from './fetchCountries.js';
+import template from '../templateCountryCard.hbs';
 // ===================================================
 
 userCountry.addEventListener('input', _.debounce(searchCountry, 500));
 
+function searchCountry() {
+  let userCountryText = userCountry.value.toLowerCase();
 
-function searchCountry (){
-    let userCountryText = userCountry.value.toLowerCase()
-
-    fetchCountries(userCountryText)
-    .then(result => {
-        console.log(result);
-        if (result.length === 1) {
-            listCountry.innerHTML = '';
-            console.log("!!!!1 elem!!!!");
-            createCountryBlock (result)
-        } else {
-
-        listMarkup(result)
-        }
-    })
-    
-    
-}
-function createCountryBlock (result){
-    const oneCountry = result[0];
-    console.log(oneCountry);
-    console.log(oneCountry.name);
-
-
+  fetchCountries(userCountryText).then(result => {
+    console.log(result);
+    if (result.length === 1) {
+      listCountry.innerHTML = '';
+      
+      createCountryBlock(result);
+    } else {
+        cardCountry.innerHTML = ''
+      listMarkup(result);
+      
+    }
+  });
 }
 
-function listMarkup (countriesArr){
-    const countryName = countriesArr.map(country => {
-        console.log(country.name.common);
-       const markup =`<li class="elem-country"><a class="link-country link" href="">${country.name.common}</a></li>`
-       return markup
-    });
-   
-    listCountry.innerHTML = countryName.join(' ')
+function createCountryBlock(result) {
+  const oneCountry = result[0];
+  console.log(oneCountry);
+  console.log(oneCountry.name.common);
+
+  let cardMarkap = template(oneCountry);
+  console.log(cardMarkap);
+  cardCountry.innerHTML = cardMarkap;
+}
+
+function listMarkup(countriesArr) {
+    ;
+  const countryName = countriesArr.map(country => {
+    const markup = `<li class="elem-country"><a class="link-country link" href="">${country.name.common}</a></li>`;
+    return markup;
+  });
+  listCountry.innerHTML = countryName.join(' ');
 }
